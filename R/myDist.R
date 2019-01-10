30e497502a04d1fe140987d9c7dfff8fcfa31ea7
poldrack <poldrack@gmail.com>
2019-01-10 14:17:49 -0800

    add missing -1

--- R/myDist.R
+++ R/myDist.R
@@ -1,6 +1,6 @@
 myDist <- function(d,method='euclidean') {
   if (method == 'abscorr') {
-    return(as.dist(abs(cor(t(d),method="pearson"))))
+    return(as.dist(1 - abs(cor(t(d),method="pearson"))))
   } else{
     return(stats::dist(d,method=method))
   }
